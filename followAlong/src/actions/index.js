f23dcdd5bd047a2353e3fe37225618e2bb250981
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-01 14:00:16 -0400

    lecture work-in-progress

--- followAlong/src/actions/index.js
+++ followAlong/src/actions/index.js
@@ -1,6 +1,11 @@
 export const TOGGLE_EDITING = "TOGGLE_EDITING";
+export const CHANGE_TITLE_INPUT = "CHANGE_TITLE_INPUT";
 
 export const toggleEditing = () => {
     console.log("2. Action Creator called.");
     return({type: TOGGLE_EDITING});
+}
+
+export const changeTitleInput = (input) => {
+    return({type: CHANGE_TITLE_INPUT, payload: input});
 }
