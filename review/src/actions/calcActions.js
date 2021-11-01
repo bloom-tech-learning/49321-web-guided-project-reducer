315180389a79c0c9e70978c9ffa50316dd1a9e72
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-01 13:14:39 -0400

    lecture work-in-progress

---
+++ review/src/actions/calcActions.js
@@ -0,0 +1,15 @@
+export const addAction = (value)=>{
+    return({type:"ADD", payload: value});
+}
+
+export const subAction = (value)=>{
+    return({type:"SUBTRACT", payload: value});
+}
+
+export const clearAction = ()=>{
+    return({type:"CLEAR"});
+}
+
+export const addToMemoryAction = ()=>{
+    return({type:"ADD_TO_MEMORY"});
+}
